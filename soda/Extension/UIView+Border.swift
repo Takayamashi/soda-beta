5ff76a3a9a4980d6c710829e945580ce1c2b286c
Takayamashi <ryu1208.takayama@gmail.com>
2018-05-31 11:30:55 +0900

    :lipstick:

---
+++ soda/Extension/UIView+Border.swift
@@ -0,0 +1,43 @@
+//
+//  UIView+Border.swift
+//  soda
+//
+//  Created by Ryuichi Takayama on 2018/05/29.
+//  Copyright © 2018年 Ryuichi Takayama. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
