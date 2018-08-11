370317673c4538a03ebf2539d536b4f1876bf9f0
Takayamashi <ryu1208.takayama@gmail.com>
2018-08-11 12:02:43 +0900

    :recycle: refact file for responsive

---
+++ soda/Class/ResponsivelineChange.swift
@@ -0,0 +1,9 @@
+//
+//  ResponsivelineChange.swift
+//  soda
+//
+//  Created by Ryuichi Takayama on 2018/08/11.
+//  Copyright © 2018年 Ryuichi Takayama. All rights reserved.
+//
+
+import Foundation
