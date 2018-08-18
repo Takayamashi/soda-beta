ae415a6f341834ab9203bc8dd24401251e96a7b7
Takayamashi <ryu1208.takayama@gmail.com>
2018-08-18 10:48:43 +0900

    :tada: add button like meterial design

---
+++ soda/Class/MaterialButton.swift
@@ -0,0 +1,67 @@
+//
+//  MaterialButton.swift
+//  soda
+//
+//  Created by Ryuichi Takayama on 2018/08/17.
+//  Copyright © 2018年 Ryuichi Takayama. All rights reserved.
+//
+
+import UIKit
+
+
+class MaterialButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        touchStartAnimation()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        touchEndAnimation()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        touchEndAnimation()
+    }
+}
+
+
+// MARK: - Private functions
+extension MaterialButton {
+    
+    //影付きのボタンの生成
+    internal func commonInit(){
+        self.layer.shadowOffset = CGSize(width: 1, height: 1 )
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowRadius = 5
+        self.layer.shadowOpacity = 1.0
+    }
+    
+    //ボタンが押された時のアニメーション
+    internal func touchStartAnimation() {
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {() -> Void in
+            self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95);
+            self.alpha = 0.9
+        },completion: nil)
+    }
+    
+    //ボタンから手が離れた時のアニメーション
+    internal func touchEndAnimation() {
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {() -> Void in
+            self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0);
+            self.alpha = 1
+        },completion: nil)
+    }
+}
