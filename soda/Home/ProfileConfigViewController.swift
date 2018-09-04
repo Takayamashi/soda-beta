5bf66a181f5f5903c7a4966df52a4e5ed9d0082b
Takayamashi <ryu1208.takayama@gmail.com>
2018-09-04 09:35:47 +0900

    :+1: add circle triming image

--- soda/Home/ProfileConfigViewController.swift
+++ soda/Home/ProfileConfigViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 import SlideMenuControllerSwift
+import RSKImageCropper
 
 
-class ProfileConfigViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
-
+class ProfileConfigViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate, RSKImageCropViewControllerDelegate{
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
     @IBOutlet weak var univaddTextField: UnivPickerTextField!
     @IBOutlet weak var univTextField: UITextField!
+    
+    @IBOutlet weak var mailAdressTextField: UITextField!
+    
+    @IBOutlet weak var ProfileTextView: TextViewLine!
+    
+    @IBOutlet weak var twitterAccountName: UITextField!
+    @IBOutlet weak var facebookAccountName: UITextField!
+    
+    
     var mainViewController: UIViewController!
     
     
@@ -31,9 +46,51 @@ class ProfileConfigViewController: UIViewController, UIImagePickerControllerDele
         
         //ナビゲーションバーの右側にボタン付与
         self.navigationItem.leftBarButtonItem = myLeftButton
+        
+        setProfile()
 
         self.setnavigationBar()
     }
+    
+    
+    func setProfile(){
+        if UserDefaults.standard.object(forKey: "profileImage") != nil{
+            //Profile画像参照
+            /*
+            let decodeData = (base64Encoded: dict["profileImage"])
+            let decodedData = NSData(base64Encoded: decodeData as! String, options: NSData.Base64DecodingOptions.ignoreUnknownCharacters)
+            let decodedImage = UIImage(data:decodedData! as Data)
+            nowprofileImage = decodedImage!
+            */
+            
+            //profileImageView.image = UserDefaults.standard.object(forKey: "profileImage") as? UIImage
+        }
+        
+        if UserDefaults.standard.object(forKey: "username") != nil{
+            userNameTextField.text = UserDefaults.standard.object(forKey: "username") as? String
+        }
+        
+        if UserDefaults.standard.object(forKey: "indicateuniv") != nil{
+            univTextField.text = UserDefaults.standard.object(forKey: "indicateuniv") as? String
+        }
+        
+        if UserDefaults.standard.object(forKey: "mailadress") != nil{
+            mailAdressTextField.text = UserDefaults.standard.object(forKey: "mailadress") as? String
+        }
+        
+        if UserDefaults.standard.object(forKey: "profile") != nil{
+            ProfileTextView.text = UserDefaults.standard.object(forKey: "profile") as? String
+        }
+        
+        if UserDefaults.standard.object(forKey: "twitteraccount") != nil{
+            twitterAccountName.text = UserDefaults.standard.object(forKey: "twitteraccount") as? String
+        }
+        
+        if UserDefaults.standard.object(forKey: "facebookaccount") != nil{
+            facebookAccountName.text = UserDefaults.standard.object(forKey: "facebookaccount") as? String
+        }
+    }
+    
     
     func setnavigationBar(){
         //NavigationBarが半透明かどうか
@@ -51,6 +108,57 @@ class ProfileConfigViewController: UIViewController, UIImagePickerControllerDele
         //self.navigationItem.titleView = UIImageView(image:UIImage(named:"logo_white_small"))
         
     }
+    
+    @IBAction func changeProfileView(_ sender: Any) {
+        let alertViewController = UIAlertController(title: "プロフィール写真を設定します", message: "", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "カメラで撮影する", style: .default, handler:{(action:UIAlertAction?) -> Void in
+            //処理
+            self.openCamera()
+        })
+        let photoAction = UIAlertAction(title: "ライブラリから選択する", style: .default, handler:{(action:UIAlertAction?) -> Void in
+            //処理
+            self.openPhoto()
+        })
+        
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        
+        alertViewController.addAction(cameraAction)
+        alertViewController.addAction(photoAction)
+        alertViewController.addAction(cancelAction)
+        present(alertViewController, animated: true, completion: nil)
+        
+        
+    }
+    
+    //　撮影が完了時した時に呼ばれる
+    func imagePickerController(_ imagePicker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image : UIImage = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
+        
+        imagePicker.dismiss(animated: false, completion: { () -> Void in
+            
+            var imageCropVC : RSKImageCropViewController!
+            
+            imageCropVC = RSKImageCropViewController(image: image, cropMode: RSKImageCropMode.circle)
+            
+            imageCropVC.moveAndScaleLabel.text = "切り取り範囲を選択"
+            imageCropVC.cancelButton.setTitle("キャンセル", for: .normal)
+            imageCropVC.chooseButton.setTitle("完了", for: .normal)
+            imageCropVC.delegate = self
+            
+            self.present(imageCropVC, animated: true)
+            
+        })
+        
+    }
+    
+    // 撮影がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
     
     @IBAction func adduniv(_ sender: Any) {
         
@@ -72,6 +180,25 @@ class ProfileConfigViewController: UIViewController, UIImagePickerControllerDele
     
     @IBAction func saveprofile(_ sender: Any) {
         
+        var data: NSData = NSData()
+        if let image = profileImageView.image{
+            data = UIImageJPEGRepresentation(image, 0.1)! as NSData
+        }
+        let profileImage64String = data.base64EncodedString(options: NSData.Base64EncodingOptions.lineLength64Characters) as String
+        let username = userNameTextField.text
+        let indicateuniv = univTextField.text
+        
+        //アプリ内へ保存
+        UserDefaults.standard.set(profileImage64String, forKey: "profileImage")
+        UserDefaults.standard.set(userNameTextField.text, forKey: "username")
+        UserDefaults.standard.set(univTextField.text, forKey: "indicateuniv")
+        UserDefaults.standard.set(mailAdressTextField.text, forKey: "mailadress")
+        UserDefaults.standard.set(ProfileTextView.text, forKey: "profile")
+        UserDefaults.standard.set(twitterAccountName.text, forKey: "twitteraccount")
+        UserDefaults.standard.set(facebookAccountName.text, forKey: "facebookaccount")
+        
+        
+        
     }
     
     
@@ -88,14 +215,20 @@ class ProfileConfigViewController: UIViewController, UIImagePickerControllerDele
     }
     
     func openPhoto(){
-        let sourceType:UIImagePickerControllerSourceType = UIImagePickerControllerSourceType.photoLibrary
+        let sourceType:UIImagePickerControllerSourceType =
+            UIImagePickerControllerSourceType.photoLibrary
         
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.photoLibrary){
             // インスタンスの作成
             let cameraPicker = UIImagePickerController()
             cameraPicker.sourceType = sourceType
             cameraPicker.delegate = self
             self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        else{
+            print("error")
             
         }
     }
@@ -111,6 +244,36 @@ class ProfileConfigViewController: UIViewController, UIImagePickerControllerDele
             self.present(cameraPicker, animated: true, completion: nil)
             
         }
+    }
+    
+    
+    func imageCropViewController(_ controller: RSKImageCropViewController, didCropImage croppedImage: UIImage, usingCropRect cropRect: CGRect, rotationAngle: CGFloat) {
+        
+        
+        //もし円形で画像を切り取りし、その画像自体を加工などで利用したい場合
+        if controller.cropMode == .circle {
+            UIGraphicsBeginImageContext(croppedImage.size)
+            let layerView = UIImageView(image: croppedImage)
+            layerView.frame.size = croppedImage.size
+            layerView.layer.cornerRadius = layerView.frame.size.width * 0.5
+            layerView.clipsToBounds = true
+            let context = UIGraphicsGetCurrentContext()!
+            layerView.layer.render(in: context)
+            let capturedImage = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            let pngData = UIImagePNGRepresentation(capturedImage)!
+            //このImageは円形で余白は透過です。
+            let png = UIImage(data: pngData)!
+            profileImageView.image = png
+            
+            dismiss(animated: true, completion: nil)
+            
+        }
+    }
+    
+    //トリミング画面でキャンセルを押した時
+    func imageCropViewControllerDidCancelCrop(_ controller: RSKImageCropViewController) {
+        dismiss(animated: true, completion: nil)
     }
     
     
